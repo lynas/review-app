d67672ad8f1ca53a0b8eb08f199c49cd3cf3fa0d
sazzad <sazzad.islam@dsinnovators.com>
2018-02-22 13:23:51 +0600

    swagger api doc added

--- src/main/java/com/lynas/Application.java
+++ src/main/java/com/lynas/Application.java
@@ -2,10 +2,35 @@ package com.lynas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(generateApiInfo());
+	}
+
+
+	private ApiInfo generateApiInfo() {
+		return new ApiInfo("Review App", "Review app for II", "1.0",
+				"NA", "szlynas@gmail.com", "NA", "NA");
 	}
 }
